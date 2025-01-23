98d29dfea79512d674fd88cd77be19de743757f1
MatiasSargo <165160391+MatiasSargo@users.noreply.github.com>
2025-01-23 15:34:33 +0100

    connect DB to mongocluster

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,20 @@ import { Module } from "@nestjs/common";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { UsersModule } from "./users/users.module";
 import { MongooseModule } from "@nestjs/mongoose";
-import { CommonModule } from './common/common.module';
-
+import { CommonModule } from "./common/common.module";
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, "..", "public"),
     }),
-    MongooseModule.forRoot("mongodb://localhost:27017/sherpapp-mongodb"),
+    MongooseModule.forRoot(
+      "mongodb+srv://matisargo:OWHtedoTp8gCz5PI@cluster0.ay2g7.mongodb.net/sherpapp",
+    ),
 
     UsersModule,
 
     CommonModule,
-
   ],
   controllers: [],
   providers: [],
