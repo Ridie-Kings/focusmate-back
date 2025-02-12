c65b72faf65b15233cb092ae642e715517ec91da
MatiasSargo <165160391+MatiasSargo@users.noreply.github.com>
2025-02-12 17:26:45 +0100

    change accesToken age to 12hs

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,17 @@
 import {
   Controller,
   Post,
+  Body,
   Req,
   Res,
-  Body,
   UnauthorizedException,
 } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { Public } from "./decorators/public.decorator";
+import { CreateUserDto } from "src/users/dto/create-user.dto";
+import { LoginUserDto } from "src/users/dto/login-user.dto";
 import { Request, Response } from "express";
 import { ApiTags, ApiOperation, ApiResponse } from "@nestjs/swagger";
-import { CreateUserDto, LoginUserDto } from "src/users/dto";
 
 @ApiTags("Authentication")
 @Controller("auth")
@@ -31,26 +32,36 @@ export class AuthController {
 
   @Public()
   @Post("login")
-  async login(@Body() loginUserDto: LoginUserDto) {
+  async login(
+    @Body() loginUserDto: LoginUserDto,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const { access_token, refresh_token } =
       await this.authService.login(loginUserDto);
-    return {
-      message: "User authenticated successfully",
-      accessToken: access_token,
-      refreshToken: refresh_token,
-      expiresIn: "12h",
-    };
+
+    res.cookie("access_token", access_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 12 * 60 * 60 * 1000, //12 hs
+    });
+
+    res.cookie("refresh_token", refresh_token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 días
+    });
+
+    return { message: "Login successful" };
   }
 
-  @ApiOperation({ summary: "Login with an existing user" })
-  @ApiResponse({ status: 200, description: "User authenticated successfully" })
-  @ApiResponse({
-    status: 401,
-    description: "Unauthorized - Invalid credentials",
-  })
   @Public()
   @Post("refresh")
-  async refresh(@Req() req: Request, @Res() res: Response) {
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const refreshToken = req.cookies?.refresh_token;
     if (!refreshToken) throw new UnauthorizedException("Refresh token missing");
 
@@ -63,18 +74,21 @@ export class AuthController {
       maxAge: 15 * 60 * 1000, // 15 min
     });
 
-    return res.json({ message: "Token refreshed" });
+    return { message: "Token refreshed" };
   }
 
   @Public()
   @Post("logout")
-  async logout(@Req() req: Request & { cookies: any }, @Res() res: Response) {
+  async logout(
+    @Req() req: Request & { cookies: any },
+    @Res({ passthrough: true }) res: Response,
+  ) {
     const refreshToken = req.cookies?.refresh_token;
     if (!refreshToken)
       throw new UnauthorizedException("No refresh token found");
 
     await this.authService.logout(refreshToken, res);
 
-    return res.json({ message: "Logged out successfully" });
+    return { message: "Logged out successfully" };
   }
 }
