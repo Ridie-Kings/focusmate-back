90603a9f3d85a51a7392b4321b66adcd03000be8
juanandub <juanandub@outlook.es>
2025-02-23 15:21:56 +0100

    fixed addword, deleteword, addshareduser and deleteshareduser (DICTS)

--- src/dicts/dto/add-delete-word.dto.ts
+++ src/dicts/dto/add-delete-word.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsNotEmpty, IsOptional, IsString } from "class-validator";
 
-export class AddWordDto {
+export class AddDeleteWordDto {
   
   @ApiProperty({ description: 'Word to be added to the dictionary', example: 'word' })
   @IsString()
@@ -11,10 +11,10 @@ export class AddWordDto {
   @ApiProperty({ description: 'Meaning of the word to be added', example: 'meaning' })
   @IsString()
   @IsNotEmpty()
-  readonly meaning: string;
+  readonly definition: string;
 
   @ApiProperty({ description: 'Example of the word to be added', example: 'example' })
-  @IsBoolean()
   @IsOptional()
-  readonly example?: boolean;
+  @IsString()
+  readonly example?: string;
 }
