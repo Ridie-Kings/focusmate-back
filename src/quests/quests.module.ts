b9f148a9c6b26a1e549c5f16829bfa4496e7842c
juanandub <juanandub@outlook.es>
2025-03-21 12:41:39 +0100

    feat: Quests module

--- src/quests/quests.module.ts
+++ src/quests/quests.module.ts
@@ -3,6 +3,8 @@ import { QuestsService } from './quests.service';
 import { QuestsController } from './quests.controller';
 import { Mongoose } from 'mongoose';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Quest, QuestSchema } from './entities/quest.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [QuestsController],
