143b4d73e1a029b21361e5c00cdddbc725c66b60
juanandub <juanandub@outlook.es>
2025-03-21 12:37:07 +0100

    feat: Quests service

--- src/quests/quests.service.ts
+++ src/quests/quests.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import mongoose, { Model } from 'mongoose';
 import { Quest } from './entities/quest.entity';
 import { InjectModel } from '@nestjs/mongoose';
@@ -15,7 +15,29 @@ export class QuestsService {
     return await this.questModel.find().populate('reward').populate('badge').populate('tasks');;
   }
 
-  findOne(id: mongoose.Types.ObjectId) {
-    return `This action returns a #${id} quest`;
+  async findOne(id: mongoose.Types.ObjectId) {
+    const quest = await this.questModel.findById(id).populate('reward').populate('badge').populate('tasks');
+    if(!quest) throw new NotFoundException('Quest not found');
+    return quest;
   }
+
+  async findQuestsByCategory(category: string): Promise<Quest[]> {
+    const quests = await this.questModel.find({category}).populate('reward').populate('badge').populate('tasks');
+    if(!quests) throw new NotFoundException('Quests not found');
+    return quests;
+  }
+
+  async searchQuest(title: string): Promise<Quest> {
+    const quest = await this.questModel.findOne({title}).populate('reward').populate('badge').populate('tasks');
+    if(!quest) throw new NotFoundException('Quest not found');
+    return quest;
+  }
+
+  async findQuestsByLevel(level: number): Promise<Quest[]> {
+    const quests = await this.questModel.find({level}).populate('reward').populate('badge').populate('tasks');
+    if(!quests) throw new NotFoundException('Quests not found');
+    return quests;
+  }
+  
+
 }
