1e052c98a79fbc8fceed3b9a5db8df04f20b9857
juanandub <juanandub@outlook.es>
2025-03-31 17:56:41 +0200

    feat: CheckLists

--- src/checklists/dto/update-checklist.dto.ts
+++ src/checklists/dto/update-checklist.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateChecklistDto } from './create-checklist.dto';
+import { IsArray, IsMongoId, IsOptional } from 'class-validator';
+import mongoose from 'mongoose';
 
-export class UpdateChecklistDto extends PartialType(CreateChecklistDto) {}
+export class UpdateChecklistDto extends PartialType(CreateChecklistDto) {
+  
+  @ApiProperty({description: 'list of task to be eliminated', example: ['60d5f484f1b2c8a4d8e4b8c1', '60d5f484f1b2c8a4d8e4b8c2']})
+  @IsArray()
+  @IsMongoId({ each: true })
+  @IsOptional()
+  taskToDelete?: mongoose.Types.ObjectId[];
+}
