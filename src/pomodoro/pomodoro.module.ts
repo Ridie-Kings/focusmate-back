2a462a7fc5f810c1767935f639e2aae55479c9a8
Ridie-Kings <fran6salvatierra@gmail.com>
2025-04-06 15:36:22 +0200

    fix

--- src/pomodoro/pomodoro.module.ts
+++ src/pomodoro/pomodoro.module.ts
@@ -1,19 +1,19 @@
-import { Module } from '@nestjs/common';
-import { PomodoroService } from './pomodoro.service';
-import { PomodoroController } from './pomodoro.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { AuthModule } from 'src/auth/auth.module';
-import { Pomodoro, PomodoroSchema } from './entities/pomodoro.entity';
-import { PomodoroGateway } from './pomodoro.gateway';
+import { Module } from "@nestjs/common";
+import { PomodoroService } from "./pomodoro.service";
+// import { PomodoroController } from './pomodoro.controller';
+import { MongooseModule } from "@nestjs/mongoose";
+import { AuthModule } from "src/auth/auth.module";
+import { Pomodoro, PomodoroSchema } from "./entities/pomodoro.entity";
+import { PomodoroGateway } from "./pomodoro.gateway";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Pomodoro.name, schema: PomodoroSchema } // Asegúrate de que el nombre y el esquema sean correctos
+      { name: Pomodoro.name, schema: PomodoroSchema }, // Asegúrate de que el nombre y el esquema sean correctos
     ]),
     AuthModule,
   ], // Aquí puedes importar otros módulos si es necesario
-  controllers: [PomodoroController],
+  // controllers: [PomodoroController],
   providers: [PomodoroService, PomodoroGateway], // Asegúrate de que PomodoroGateway esté importado y registrado aquí
   exports: [PomodoroService], // Exporta el servicio si lo necesitas en otros módulos
 })
